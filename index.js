c076e62589810d094f6b48e136abdb3ca0652bb4
nikitakozlovjr <nikita3726898@gmail.com>
2023-10-31 08:44:17 +0300

    Привели файл index.js к чистому виду

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 import Validator from './src/Validator.js';
-const validator = new Validator();
-const schema1 = validator.string().startsFromUpperCase();
 
-console.log(schema1.isValid('Hexlet'));
 export default Validator;
