b9160466a4e05e66d5f8fd22251ce2893f13aa38
nikitakozlovjr <nikita3726898@gmail.com>
2023-10-31 08:39:42 +0300

    Расширили функциональность валидатора array. Теперь массив можно проверить на максимальную вложенность

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -4,6 +4,14 @@ class Validator {
 
     validators = [Validator.isArray];
 
+    maxDepth(maxDepth) {
+        const getDepth = (value) => Validator.isArray(value) ? 1 + Math.max(0, ...value.map(getDepth)) : 0;
+        this.validators.push((value) => {
+            console.log(getDepth(value))
+            return getDepth(value) - 1 <= maxDepth
+        });
+        return this;
+    }
     isValid(value) {
         const result = this.validators.map((validator) => validator(value));
         return !result.includes(false);
